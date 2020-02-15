68e01dd367e715c6eea79ba3c0832e39c1fe1b6e
louisdalligos <zindex14@gmail.com>
2020-02-15 14:36:10 +0700

    Initial setup

---
+++ pages/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Head from "next/head";
+import Nav from "../components/nav";
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <div className="hero">
+      <h1 className="title">Welcome to Unity Pro Designs!</h1>
+      <p className="description">
+        We are working on our homepage and will be available soon.
+      </p>
+    </div>
+
+    <style jsx>{`
+      :global(body) {
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
+          Helvetica, sans-serif;
+      }
+      .hero {
+        width: 100%;
+        color: #333;
+      }
+      .title {
+        margin: 0;
+        width: 100%;
+        padding-top: 80px;
+        line-height: 1.15;
+        font-size: 48px;
+      }
+      .title,
+      .description {
+        text-align: center;
+      }
+      .row {
+        max-width: 880px;
+        margin: 80px auto 40px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+      }
+    `}</style>
+  </div>
+);
+
+export default Home;
